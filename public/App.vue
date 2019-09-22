5d4f014fea72abfcee03a3ff0ac7e553587a7d49
Ailton Moura <ailton.p.moura@gmail.com>
2019-09-22 14:34:39 -0300

    Configuracao do projeto, adicionados as dependencias webpack, babel, vue e sass

---
+++ public/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  data () {
+    return {
+      msg: 'Init Project'
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+@import '~styles/style';
+
+div {
+  text-align: $text-align;
+  font-size: $font-size;
+  font-family: $font-family;
+}
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path')
+var VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  entry: './public/javascripts/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }, 
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+    ]
+  },
+
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      'styles': path.resolve(__dirname, './public/stylesheets')
+    },
+  },
+};
